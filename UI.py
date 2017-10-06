1bf94997ab93b51308b1ffdaef38aa87c55b97ca
jamesleu <31709298+jamesleu@users.noreply.github.com>
2017-10-06 18:54:58 -0400

    CSV
    Import CSV coordinates

--- UI.py
+++ UI.py
@@ -1,11 +1,15 @@
 import sys
 import vtk
+import csv
+import xlrd
 from PyQt4 import QtCore, QtGui
 from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
-class UI(QtGui.QMainWindow):
 
+class UI(QtGui.QMainWindow):
     solid = 1
+    global points
+    points = {}
 
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
@@ -20,24 +24,11 @@ class UI(QtGui.QMainWindow):
         self.vtkWidget.GetRenderWindow().AddRenderer(self.render)
         self.interactor = self.vtkWidget.GetRenderWindow().GetInteractor()
 
-
-        mapper = vtk.vtkPolyDataMapper()
-
-##        #Reads .obj file and renders into window
-##        filename1 = "millenium.obj"
-##
-##        reader = vtk.vtkOBJReader()
-##        reader.SetFileName(filename1)
-##
-##        mapper = vtk.vtkPolyDataMapper()
-##        if vtk.VTK_MAJOR_VERSION <= 5:
-##            mapper.SetInput(reader.GetOutput())
-##        else:
-##            mapper.SetInputConnection(reader.GetOutputPort())
+        self.mapper = vtk.vtkPolyDataMapper()
 
         # Create an actor
         self.actor = vtk.vtkActor()
-        self.actor.SetMapper(mapper)
+        self.actor.SetMapper(self.mapper)
 
         self.render.AddActor(self.actor)
 
@@ -49,29 +40,101 @@ class UI(QtGui.QMainWindow):
         self.show()
         self.interactor.Initialize()
 
-        #Import button
+        # Import button
         self.planes = QtGui.QPushButton('Import Plane Model', self)
         self.layout.addWidget(self.planes)
         self.planes.clicked.connect(self.readfiles)
 
-        #Wireframe button
+        # Wireframe button
         self.wireframe = QtGui.QPushButton('Toggle Wireframe Mode', self)
         self.layout.addWidget(self.wireframe)
         self.wireframe.clicked.connect(self.handleButton)
 
+        # ids
+        self.colorPlane = QtGui.QPushButton('Color Plane', self)
+        self.layout.addWidget(self.colorPlane)
+        self.colorPlane.clicked.connect(self.addColors)
+
+        self.pd = vtk.vtkPolyData()
+
+        # CSV Antenna Button
+        self.antenna = QtGui.QPushButton('Get CSV', self)
+        self.layout.addWidget(self.antenna)
+        self.antenna.clicked.connect(self.importCSV)
+        
+## <---------------------------------------------------------------------------------------->
+
+    # read sample XLS sent
+##    def readXLS(self):
+##        fileXLS = xlrd.open_workbook('F16.xls')
+##        coordinates = fileXLS.sheet_by_index(0)
+##        
+##        xyz = []
+##        #xyz =[] #list of xyz in each row
+##        coords = [] #list of all coordinates
+##        for i in range(0, 9):
+##            xyz.append([])
+##            for j in range(0, 3):
+##                xyz[i].append(coordinates.cell(i , j))
+##        print "coordinate list: ", xyz
+        
+    # read and print CSV file
+    def readCSV(self, antennas):
+        xyz = []
+        with open(antennas, 'rb') as csvfile:
+            coordinates = csv.reader(csvfile, delimiter = ',')
+            for row in coordinates:
+                #convert list of strings to float
+                row = map(float, row) 
+                xyz.append(row)
+            print xyz #testing print
+        return xyz
+
+    # import CSV files for antenna coordinates
+    def importCSV(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self, "Import CSV files")
+        file = open(filename, "r")
+        self.readCSV(filename)
+
+
+    def addColors(self):
+        #Tuple at index (i) denotes the color of point(i)
+        colors = vtk.vtkUnsignedCharArray()
+        colors.SetNumberOfComponents(3)
+
+        #Goes through each point in the polydata and assigns a color based on some criteria
+        for i in range(1, self.pd.GetNumberOfPoints()+1):
+            if i%3 == 0:
+                colors.InsertNextTuple3(255,0,0)
+            elif i%3 == 1:
+                colors.InsertNextTuple3(0,255,0)
+            else:
+                colors.InsertNextTuple3(0,0,255)
+            #sets up dict to track point ids by their coordinates
+            points[self.pd.GetPoint(i)] = i
+
+        #required to show colors on the actor
+        self.pd.GetPointData().SetScalars(colors)
+        self.mapper.SetColorModeToDefault()
+        self.mapper.SetScalarModeToUsePointData()
+        self.interactor.Render()
+
+    # Add 3D Models
     def addModel(self, reader):
 
-        mapper = vtk.vtkPolyDataMapper()
+        self.mapper = vtk.vtkPolyDataMapper()
         if vtk.VTK_MAJOR_VERSION <= 5:
-            mapper.SetInput(reader.GetOutput())
+            self.mapper.SetInput(reader.GetOutput())
         else:
-            mapper.SetInputConnection(reader.GetOutputPort())
+            self.mapper.SetInputConnection(reader.GetOutputPort())
 
-            self.actor.SetMapper(mapper)
+            self.actor.SetMapper(self.mapper)
             self.render.AddActor(self.actor)
+            self.pd = self.mapper.GetInput()
             self.render.ResetCamera()
-            self.interactor.Render()    
-              
+            self.interactor.Render()
+            
+    # Handle Wireframe
     def handleButton(self):
         if self.solid == 1:
             self.actor.GetProperty().SetRepresentationToWireframe()
@@ -82,32 +145,32 @@ class UI(QtGui.QMainWindow):
             self.interactor.Render()
             self.solid = 1
 
-    #importing OBJ/STL currently
+
+    # importing OBJ/STL/PLY currently
     def readfiles(self):
         filename = QtGui.QFileDialog.getOpenFileName(self, "Import Models")
         file = open(filename, "r")
         extension = QtCore.QFileInfo(filename).suffix()
-        
+
         if extension == 'obj':
-            
             with file:
-                
                 reader = vtk.vtkOBJReader()
                 reader.SetFileName(str(filename))
                 self.addModel(reader)
 
-
         if extension == 'stl':
-
             with file:
-                
                 reader = vtk.vtkSTLReader()
                 reader.SetFileName(str(filename))
                 self.addModel(reader)
 
+        if extension == 'ply':
+            with file:
+                reader = vtk.vtkPLYReader()
+                reader.SetFileName(str(filename))
+                self.addModel(reader)
 
-
-
+                
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
 
